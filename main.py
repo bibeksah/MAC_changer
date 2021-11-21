bc2a0d94ad8a037525766f22069f052ac9fc94cd
bibek <bobeksah36@gmail.com>
2021-11-21 10:27:35 +0530

    main code

--- main.py
+++ main.py
@@ -2,17 +2,30 @@
 
 import subprocess
 import optparse
+import re
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="interface to be getting new mac address")
-parser.add_option("-m", "--mac", dest="New_mac", help="to enter new mac address")
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest=" interface", help="interface to be getting new mac address")
+    parser.add_option("-m", "--mac", dest=" New_mac", help="to enter new mac address")
+    return parser.parse_args()
 
-(options, arguments) = parser.parse_args()
-interface = options.interface
-new_mac = options.new_mac
+def change_mac(interface, new_mac):
+    print("[+] changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-print("[+] changing MAC address for " + interface + " to " + new_mac)
+def current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.serach(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-]  Could not read MAC address. ")
+
+(options,arguments) = get_arguments()
+c_mac = current_mac(options.interface)
+print("current MAC = ", str(c_mac))
+change_mac(options.interface, options.new_mac)
